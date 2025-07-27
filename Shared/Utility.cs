beca87debe7036e2a8c1fba4685d19397d629ebb
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2025-07-27 19:49:49 +0430

    API Url updated

--- Shared/Utility.cs
+++ Shared/Utility.cs
@@ -9,7 +9,8 @@ namespace Shared
     public static class Utility
     {
         public static string BlazorServer_URI { get; set; } = "https://localhost:7217/";
-        public static string API_URI { get; set; } = "https://localhost:7210/";
+        //public static string API_URI { get; set; } = "https://localhost:7210/";
+        public static string API_URI { get; set; } = "https://exchangeappbackend.onrender.com/";
 
     }
 }
